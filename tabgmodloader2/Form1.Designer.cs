7f8979ae7bc64b84f1967d089ea276cef904df77
mhnd4414 <54472205+mhnd4414@users.noreply.github.com>
2019-08-31 01:47:33 -0400

    initial commit

---
+++ tabgmodloader2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace tabgmodloader2
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.titlebar = new System.Windows.Forms.PictureBox();
+            this.titlebarletters = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.minButton = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.vanilla = new System.Windows.Forms.Button();
+            this.modded = new System.Windows.Forms.Button();
+            this.progressbar = new System.Windows.Forms.PictureBox();
+            this.load = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.titlebar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressbar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titlebar
+            // 
+            this.titlebar.BackColor = System.Drawing.Color.Fuchsia;
+            this.titlebar.BackgroundImage = global::tabgmodloader2.Properties.Resources._1;
+            this.titlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlebar.Location = new System.Drawing.Point(0, 0);
+            this.titlebar.Name = "titlebar";
+            this.titlebar.Size = new System.Drawing.Size(800, 16);
+            this.titlebar.TabIndex = 0;
+            this.titlebar.TabStop = false;
+            this.titlebar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Titlebar_MouseDown);
+            // 
+            // titlebarletters
+            // 
+            this.titlebarletters.BackColor = System.Drawing.Color.Fuchsia;
+            this.titlebarletters.Font = new System.Drawing.Font("Wavehaus 42 Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlebarletters.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.titlebarletters.Image = global::tabgmodloader2.Properties.Resources._1;
+            this.titlebarletters.Location = new System.Drawing.Point(0, 0);
+            this.titlebarletters.Name = "titlebarletters";
+            this.titlebarletters.Size = new System.Drawing.Size(152, 16);
+            this.titlebarletters.TabIndex = 1;
+            this.titlebarletters.Text = "TABG Mod Loader 2";
+            this.titlebarletters.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.titlebarletters.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Titlebarletters_MouseDown);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("closeButton.BackgroundImage")));
+            this.closeButton.Location = new System.Drawing.Point(776, 0);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(24, 16);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            this.closeButton.MouseLeave += new System.EventHandler(this.CloseButton_MouseLeave);
+            // 
+            // minButton
+            // 
+            this.minButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("minButton.BackgroundImage")));
+            this.minButton.Location = new System.Drawing.Point(760, 0);
+            this.minButton.Margin = new System.Windows.Forms.Padding(0);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(16, 16);
+            this.minButton.TabIndex = 3;
+            this.minButton.TabStop = false;
+            this.minButton.Click += new System.EventHandler(this.MinButton_Click);
+            this.minButton.MouseEnter += new System.EventHandler(this.MinButton_MouseEnter);
+            this.minButton.MouseLeave += new System.EventHandler(this.MinButton_MouseLeave);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 22);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(800, 330);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Url = new System.Uri("https://4chords.cc/loader/changelog.html", System.UriKind.Absolute);
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // vanilla
+            // 
+            this.vanilla.BackgroundImage = global::tabgmodloader2.Properties.Resources._3;
+            this.vanilla.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.vanilla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vanilla.Font = new System.Drawing.Font("Wavehaus 42 Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vanilla.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.vanilla.Location = new System.Drawing.Point(12, 358);
+            this.vanilla.Name = "vanilla";
+            this.vanilla.Size = new System.Drawing.Size(385, 80);
+            this.vanilla.TabIndex = 5;
+            this.vanilla.Text = "Vanilla";
+            this.vanilla.UseVisualStyleBackColor = true;
+            this.vanilla.Click += new System.EventHandler(this.Vanilla_Click);
+            // 
+            // modded
+            // 
+            this.modded.BackgroundImage = global::tabgmodloader2.Properties.Resources._3;
+            this.modded.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.modded.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.modded.Font = new System.Drawing.Font("Wavehaus 42 Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modded.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.modded.Image = global::tabgmodloader2.Properties.Resources._3;
+            this.modded.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.modded.Location = new System.Drawing.Point(403, 358);
+            this.modded.Name = "modded";
+            this.modded.Size = new System.Drawing.Size(385, 80);
+            this.modded.TabIndex = 6;
+            this.modded.Text = "Modded";
+            this.modded.UseVisualStyleBackColor = true;
+            this.modded.Click += new System.EventHandler(this.Modded_Click);
+            // 
+            // progressbar
+            // 
+            this.progressbar.Location = new System.Drawing.Point(0, 452);
+            this.progressbar.Margin = new System.Windows.Forms.Padding(0);
+            this.progressbar.Name = "progressbar";
+            this.progressbar.Size = new System.Drawing.Size(0, 5);
+            this.progressbar.TabIndex = 7;
+            this.progressbar.TabStop = false;
+            // 
+            // load
+            // 
+            this.load.AutoSize = true;
+            this.load.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.load.Image = global::tabgmodloader2.Properties.Resources._1;
+            this.load.Location = new System.Drawing.Point(183, 3);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(62, 13);
+            this.load.TabIndex = 8;
+            this.load.Text = "Loaded At: ";
+            this.load.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.load.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Load_MouseDown);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::tabgmodloader2.Properties.Resources._4;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.progressbar);
+            this.Controls.Add(this.modded);
+            this.Controls.Add(this.vanilla);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.minButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.titlebarletters);
+            this.Controls.Add(this.titlebar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.Text = "TABG Mod Loader 2";
+            ((System.ComponentModel.ISupportInitialize)(this.titlebar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressbar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox titlebar;
+        private System.Windows.Forms.Label titlebarletters;
+        private System.Windows.Forms.PictureBox closeButton;
+        private System.Windows.Forms.PictureBox minButton;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button vanilla;
+        private System.Windows.Forms.Button modded;
+        private System.Windows.Forms.PictureBox progressbar;
+        private System.Windows.Forms.Label load;
+    }
+}
+
---
+++ tabgmodloader2/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace tabgmodloader2
+{
+
+
+    public partial class main : Form
+    {
+
+        public string install;
+        public bool log = false;
+
+        public int UPDATEMEORDIE = 4;
+
+
+        public main()
+        {
+            InitializeComponent();
+            GetInstallPath();
+            GetArgs();
+            
+            if (File.Exists(Path.GetTempPath() + @"dl.dll"))
+            {
+                File.Delete(Path.GetTempPath() + @"dl.dll");
+            }
+        }
+
+
+        [DllImport("kernel32.dll")]
+        static extern bool CreateSymbolicLink(
+        string lpSymlinkFileName, string lpTargetFileName, SymbolicLink dwFlags);
+
+        enum SymbolicLink
+        {
+            File = 0,
+            Directory = 1
+        }
+        //  CreateSymbolicLink(symbolicLink, fileName, SymbolicLink.File);
+
+        public void GetArgs()
+        {
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                if (arg == "-noupdate")
+                {
+                    MessageBox.Show("Updates temporarily disabled", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    CheckUpdate();
+                }
+                if (arg == "-conlog")
+                {
+                    MessageBox.Show("Console Logging Enabled", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    log = true;
+                    AllocConsole();
+                    Log("Console Logging Enabled!");
+                }
+
+            }
+        }
+
+        public void Log(string l)
+        {
+            if (log) {
+                Console.WriteLine("[" + DateTime.UtcNow.Hour.ToString() + ":" +DateTime.UtcNow.Minute.ToString() + "." + DateTime.UtcNow.Millisecond.ToString() + "] " + l);
+            }
+        }
+
+        public void CheckUpdate()
+        {
+            int temp;
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://4chords.cc/loader/v");
+            StreamReader reader = new StreamReader(stream);
+            String content = reader.ReadToEnd();
+            int.TryParse(content, out temp);
+            if(temp != UPDATEMEORDIE)
+            {
+                DialogResult result = MessageBox.Show("An update is required before you can use TABG Mod Loader", "Update Available", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    Process.Start("https://4chords.cc/loader/download");
+                    Environment.Exit(11);
+                }
+                else
+                {
+                    Environment.Exit(12);
+                }
+            }
+        }
+
+        public void GetInstallPath()
+        {
+            RegistryKey myKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 823130", false);
+            try { 
+                install = (String)myKey.GetValue("InstallLocation");
+            } catch
+            {
+                MessageBox.Show("Game not found in Steam Directory", "Error",  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Environment.Exit(-1);
+            }
+            titlebarletters.Text += " | ";
+            load.Text += install;
+        }
+
+
+        public void Progress(int p)
+        {
+            progressbar.Width = p*800/100;
+            progressbar.BackColor = GetBlendedColor(p);
+            return;
+        }
+
+        public Color GetBlendedColor(int percentage)
+        {
+            if (percentage < 50)
+                return Interpolate(Color.Red, Color.Yellow, percentage / 50.0);
+            return Interpolate(Color.Yellow, Color.Lime, (percentage - 50) / 50.0);
+        }
+
+        private Color Interpolate(Color color1, Color color2, double fraction)
+        {
+            double r = Interpolate(color1.R, color2.R, fraction);
+            double g = Interpolate(color1.G, color2.G, fraction);
+            double b = Interpolate(color1.B, color2.B, fraction);
+            return Color.FromArgb((int)Math.Round(r), (int)Math.Round(g), (int)Math.Round(b));
+        }
+
+        private double Interpolate(double d1, double d2, double fraction)
+        {
+            return d1 + (d2 - d1) * fraction;
+        }
+
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                // The variable that will be holding the url address (making sure it starts with http://)
+                Uri URL = urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+
+                try
+                {
+                    // Start downloading the file
+                    webClient.DownloadFileAsync(URL, location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        // The event that will fire whenever the progress of the WebClient is changed
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update the progressbar percentage only when the value is not the same.
+            Progress(e.ProgressPercentage);
+        }
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                System.IO.File.Copy(Path.GetTempPath() + @"dl.dll", install + @"\TotallyAccurateBattlegrounds_Data\Managed\Assembly-CSharp.dll", true);
+                File.Delete(Path.GetTempPath() + @"dl.dll");
+                try
+                {
+                    System.Diagnostics.Process.Start(@"steam://rungameid/823130");
+                    Application.Exit();
+                }
+                catch (Exception excp)
+                {
+                    MessageBox.Show(excp.ToString(), "An Error Has Occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception excp)
+            {
+                MessageBox.Show(excp.ToString(), "An Error Has Occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        // Titlebar Dragging Stuff
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        private void Titlebar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Titlebarletters_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.BackgroundImage = tabgmodloader2.Properties.Resources.closeh;
+        }
+
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.BackgroundImage = tabgmodloader2.Properties.Resources.close;
+        }
+
+        private void MinButton_MouseEnter(object sender, EventArgs e)
+        {
+            minButton.BackgroundImage = tabgmodloader2.Properties.Resources.minh;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MinButton_MouseLeave(object sender, EventArgs e)
+        {
+            minButton.BackgroundImage = tabgmodloader2.Properties.Resources.min;
+        }
+
+        private void MinButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Load_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Vanilla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DownloadFile(@"http://github.com/wafflehammer/TabgGunGame/raw/master/latest.dll", Path.GetTempPath() + @"dl.dll");
+            }
+            catch(Exception excp)
+            {
+                MessageBox.Show(excp.ToString(), "An Error Has Occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Modded_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DownloadFile(@"http://github.com/wafflehammer/TabgGunGame/raw/master/Assembly-CSharp.dll", Path.GetTempPath() + @"dl.dll");
+            }
+            catch (Exception excp)
+            {
+                MessageBox.Show(excp.ToString(), "An Error Has Occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //console stuff
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+    }
+}
